8f72f06f69e3cb0c805561d79d0504261558be4d
Adarsh Dhakad <dhakadadarsh1@gmail.com>
2020-10-01 19:40:11 +0530

    with audio integration

--- app/src/main/java/com/example/mywhetherappadarsh/MainActivity.kt
+++ app/src/main/java/com/example/mywhetherappadarsh/MainActivity.kt
@@ -2,18 +2,21 @@ package com.example.mywhetherappadarsh
 
 
 
+import android.app.PendingIntent.getActivity
+import android.content.Context
+import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log
-import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.Toast
-
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
+import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONException
-import java.net.URL
 import java.util.*
 
 
@@ -37,48 +40,75 @@ class MainActivity : AppCompatActivity() {
     private fun find_whether() {
         val editText = findViewById<EditText>(R.id.edittext)
         cityname=editText.getText().toString()
-        Toast.makeText(this,"$cityname",Toast.LENGTH_LONG).show()
-        val myURL = "http://api.openweathermap.org/data/2.5/weather?q=$cityname&appid=c84adac79793c9ff9e676af2004580d9"
-        var url = myURL
+   //     val intent = Intent(context, testActivity::class.java)
+  //      val pendingIntent = getActivity(context, 0, intent, Intent.FLAG_ACTIVITY_NEW_TASK)
 
+        val mgr: InputMethodManager =
+           getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        mgr.hideSoftInputFromWindow(editText.getWindowToken(), 0)
+        if(cityname.isNullOrBlank()){
 
-        val jsonObjectRequest = JsonObjectRequest(
-            Request.Method.GET, url, null,
-            { response ->
-                try {
-                    val json_mainobj = response.getJSONObject("main")
-                    //    date.text = "Response: %s".format(response.toString())
-                    var humidity = json_mainobj.getString("humidity")
-                    val kelvinValue = json_mainobj.getDouble("temp")
+            Toast.makeText(this, "Please enter city name ", Toast.LENGTH_LONG).show()
 
-                    val city = response.getString("name")
-                    val Fahrenheit = (((kelvinValue - 273.15) * 9 / 5) + 32).toFloat()
-                    val celsius = (kelvinValue - 273.15).toFloat()
-
-                    //  val ab=null //= celsius.toInt()
-                    // day.text="$ab klk"
-                    //   celsius.text = "$celsius 'C"
-                    celsiusview.text = "$celsius 'C"
-                    fahrenheit.text = "$Fahrenheit'K"
-                    val date = Date()
-                    day.text = "$date"
-                    mycity.text = ("$city")
-                    humidityview.text = ("humidity $humidity % ")
-
-                } catch (e: JSONException) {
-                    Log.d("JSONException", e.localizedMessage)
-                }
-
-
-            },
-            {
-                Log.d("error", it.localizedMessage)
+        }
+           else if (cityname!!.contains(" ")) {
+            Toast.makeText(this, "Please enter valid name ", Toast.LENGTH_LONG).show()
             }
-        )
+        else {
+            Toast.makeText(this, "$cityname", Toast.LENGTH_LONG).show()
+            val myURL =
+                "http://api.openweathermap.org/data/2.5/weather?q=$cityname&appid=c84adac79793c9ff9e676af2004580d9"
+            var url = myURL
+
+
+            val jsonObjectRequest = JsonObjectRequest(
+                Request.Method.GET, url, null,
+                { response ->
+                    try {
+                        val json_mainobj = response.getJSONObject("main")
+                        //    date.text = "Response: %s".format(response.toString())
+                        var humidity = json_mainobj.getString("humidity")
+                        val kelvinValue = json_mainobj.getDouble("temp")
+
+                        val city = response.getString("name")
+                        val Fahrenheit = (((kelvinValue - 273.15) * 9 / 5) + 32).toFloat()
+                        val celsius = (kelvinValue - 273.15).toFloat()
+
+                        //  val ab=null //= celsius.toInt()
+                        // day.text="$ab klk"
+                        //   celsius.text = "$celsius 'C"
+                        celsiusview.text = "$celsius 'C"
+                        fahrenheit.text = "$Fahrenheit'K"
+                        val date = Date()
+                        day.text = "$date"
+                        mycity.text = ("$city")
+                        humidityview.text = ("humidity $humidity % ")
+                        textView2.text = ""
+
+                    } catch (e: JSONException) {
+                        Log.d("JSONException", e.localizedMessage)
+                    }
+
+                },
+
+                Response.ErrorListener {
+                    textView2.text = "You enter wrong city!"
+
+                    var mPlayer2 = MediaPlayer.create(this, R.raw.error);
+                    mPlayer2.start();
+
+                })
+
+//                  Toast.makeText(this,"you enter wrong city name",Toast.LENGTH_LONG)
+//                    Log.d("error", it.localizedMessage)
+//                //    Log.w("error in response", "Error: " + error.getMessage());
+//
+//                }
+//            )
 
 // Access the RequestQueue through your singleton class.
 
-        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
-        //   mComtext?.let { MySingleton.getInstance(it).addToRequestQueue(jsonObjectRequest) }
-
+            MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+            //   mComtext?.let { MySingleton.getInstance(it).addToRequestQueue(jsonObjectRequest) }
+        }
     } }
